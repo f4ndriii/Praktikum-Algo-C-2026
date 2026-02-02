0ad15a243a6583963261f2473df2921117b69cc4
f4ndriii <alfandri10062007@gmail.com>
2026-02-02 12:14:54 +0700

    menambahkan file hello.py dan variable.py

---
+++ Praktikum1/Hello.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+nama = 'alfandri'
+kelas = 'TI C'
+
+#Cara 1
+print('halo saya',nama, 'dari kelas',kelas)
+#Cara 2
+print(f'Halo saya {nama} dari kelas {kelas}')
+#Cara 3
+print('halo saya '+nama+' dari kelas '+kelas)
